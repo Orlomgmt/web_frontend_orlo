fdad3c2f1b7e2d93f64eb716f10a77e37093bd0b
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-07-16 10:14:34 +0100

    updated the mobile view of some sections

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import backgroundImage from "../images/blogSectionBg.png";
 import imageBLog1 from "../images/blogImage1.png";
 import profileBlogImage from "../images/Ellipse.png";
 
 const BlogSection = () => {
+  const [visibleItems, setVisibleItems] = useState(4);
+
   const blogData = [
     {
       id: 1,
@@ -102,6 +104,26 @@ const BlogSection = () => {
       date: "July 12, 2022",
     },
   ];
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1280) {
+        // xl breakpoint
+        setVisibleItems(8);
+      } else if (window.innerWidth >= 768) {
+        // md breakpoint
+        setVisibleItems(6);
+      } else {
+        setVisibleItems(4);
+      }
+    };
+
+    handleResize(); // Call once to set initial state
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section
       className=" w-[100%] flex flex-col justify-center items-center mx-auto bg-primary101 "
@@ -113,34 +135,38 @@ const BlogSection = () => {
       }}
     >
       <div className=" flex flex-col justify-start items-start w-[90%] mx-auto mt-5 overflow-x-hidden mb-12 ">
-        <h1 className=" font-Avenir font-bold text-black xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl">
+        <h1 className=" font-Avenir font-bold text-black xl:text-5xl lg:text-4xl md:text-3xl text-[32px]">
           Blog Posts
         </h1>
-        <p className=" font-Avenir xl:text-lg lg:text-base text-base font-normal text-customGray-200 mt-5">
+        <p className=" font-Avenir xl:text-lg text-base font-normal text-customGray-200 mt-5">
           Our latest updates and blogs about managing your team
         </p>
-        <div className=" w-full grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-x-6 gap-y-8 mt-16">
-          {blogData.map((item, id) => (
+        <div className=" w-full grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-x-6 gap-y-8 mt-16">
+          {blogData.slice(0, visibleItems).map((item, id) => (
             <div
               kay={id}
-              className=" w-[285px] gap-5 bg-white overflow-hidden h-[380px] flex flex-col rounded-3xl"
+              className=" lg:w-[285px] max-[625px]:w-[130px] gap-5 bg-white overflow-hidden lg:h-[380px] md:h-[340px] flex flex-col rounded-3xl"
             >
               <img src={item.image} alt={item.author} className="w-full flex" />
 
               <div className=" flex flex-col gap-3 px-5">
-                <h2 className=" font-Avenir text-customGray-300 text-xl font-medium">
+                <h2 className=" font-Avenir text-customGray-300 lg:text-xl md:text-lg max-[625px]:text-[16.891px] font-medium">
                   {item.title}
                 </h2>
-                <p className=" font-Avenir font-normal text-[12px] text-customGray-200">
+                <p className=" font-Avenir font-normal lg:text-[12px] md:text-[10px] text-[12.936px] text-customGray-200">
                   {item.content}
                 </p>
               </div>
-              <div className=" px-5 gap-4 flex flex-row justify-center items-center mt-6">
-                <img src={item.author.profileImage} alt={profileBlogImage} />
-                <div className=" flex flex-row gap-4 font-Avenir text-[9.4px] font-semibold  text-customGray-200">
-                  <p>{item.author.name}</p>
+              <div className=" w-full px-5 gap-4 flex flex-row justify-center items-center lg:mt-6 md:mt-4 mb-2">
+                <img
+                  src={item.author.profileImage}
+                  alt={profileBlogImage}
+                  className="w-6 h-6 rounded-full"
+                />
+                <div className=" flex flex-row gap-4 font-Avenir lg:text-[9.4px] text-[10.945px] font-semibold  text-customGray-200">
+                  <p className=" text-nowrap ">{item.author.name}</p>
                   <p>|</p>
-                  <p>{item.date}</p>
+                  <p className="  text-nowrap">{item.date}</p>
                 </div>
               </div>
             </div>

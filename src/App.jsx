81d0ffe6539b879d3da1bde9fc3abc78beaceffd
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-07-31 00:35:21 +0100

    done the the coming soon desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -11,35 +11,31 @@ const App = () => {
   };
 
   return (
-    <div className="w-full h-screen relative bg-gradient-to-tl select-none from-slate-50 to-blue-200 overflow-hidden sm:justify-center sm:items-start mx-auto flex flex-col px-4 sm:px-28">
-      <div className="w-full py-5 sm:py-10 sm:flex md:flex lg:flex xl:flex justify-center sm:justify-start sm:absolute sm:top-0 sm:left-28 z-50 hidden ">
+    <div className="w-full h-screen relative bg-gradient-to-tl select-none from-slate-50 to-blue-200 overflow-x-hidden mx-auto flex flex-col px-4 sm:px-28 md:justify-start md:items-start">
+      <div className="w-full py-5 sm:py-10 flex justify-center md:justify-start md:absolute md:top-0 md:left-28 z-50 ">
         <img
           src="/orloLogo.svg"
           alt="Orlo Logo"
-          className="w-32 sm:w-48 h-auto"
+          className="w-32 sm:w-48 sm:mt-[200px] md:mt-0 h-auto"
         />
       </div>
-
-      <div className="flex h-full flex-col justify-center sm:justify-start items-center sm:items-start max-w-[658px] z-40 sm:z-auto sm:mt-32">
-        <img
-          src="/orloLogo.svg"
-          alt="Orlo Logo"
-          className="w-32 sm:w-48 h-auto flex mb-4 sm:hiddedn lg:hidden xl:hidden md:hidden"
-        />
-        <h1 className="text-6xl sm:text-[134px] leading-tight sm:leading-[132px] font-AvenirBlack z-50 font-black text-[#3778C2] text-center sm:text-left">
+      <div className="flex h-full flex-col justify-center -mt-32 items-center md:items-start max-w-[658px] z-40 md:z-auto md:mt-32">
+        <h1 className="text-6xl sm:text-[134px]  font-AvenirBlack z-50 font-black text-[#3778C2] text-center md:text-left">
           Coming Soon!
         </h1>
-        <p className="text-lg text-[#1F1D21] font-Avenir font-normal text-center sm:text-left z-50">
+        <p className="text-lg text-[#1F1D21] font-Avenir font-normal text-center md:text-left z-50">
           We are currently in the process of building.
         </p>
-        <p className="text-lg text-[#1F1D21] font-Avenir font-normal text-center sm:text-left z-50">
+        <p className="text-lg text-[#1F1D21] font-Avenir font-normal text-center md:text-left z-50">
           You can subscribe to our mailing list so you want to get notified when
           we're LIVE.
         </p>
-
+        <p className="justify-self-end absolute bottom-3 text-sm font-normal opacity-70 ">
+          © Copyright Orlo | All Rights Reserved
+        </p>
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col gap-3 items-center sm:items-start mt-5 w-full max-w-[585px] z-50"
+          className="flex flex-col gap-3 items-center md:items-start mt-5 w-full max-w-[585px] z-50"
         >
           <input
             type="email"
@@ -57,21 +53,21 @@ const App = () => {
           </button>
         </form>
       </div>
-
       <img
         src="/washingM.png"
         alt="Washing Machine"
-        className="absolute -bottom-44 sm:-right-44 md:w-[650px] md:h-[650px] w-fit h-fit sm:top-0 z-10 "
+        className="absolute bottom-4 w-[350px] h-[350px] md:w-[550px] md:h-[450px] sm:bottom-[400px] md:mt-[450px] lg:mt-0  sm:left-1/2 lg:left-1/2 sm:-translate-x-1/2 md:-right-44 lg:w-[700px] lg:h-[700px] md:top-0 md:translate-x-32 z-10"
       />
       <img
         src="/Vector 344.png"
         alt="Vector"
-        className="absolute -right-1 w-[650px] h-[650px] top-0 z-0 sm:z-10"
+        className="absolute -right-1 w-[650px] h-[650px] top-0 z-0"
       />
+
       <img
         src="/construstionTape.png"
         alt="Construction Tape"
-        className="absolute right-[90px] top-0 w-[800px] md:w-fit md:-right-[500px] lg:-bottom-[260px] md:-bottom-[260px] xl:-bottom-[260px] -rotate-[40deg] z-20"
+        className="absolute right-[0.5px] max-w-[414px]:top-4 w-full md:w-fit md:-right-[260px] lg:-bottom-[260px] md:-bottom-[260px] xl:-bottom-[260px] md:-rotate-[40deg] z-20"
       />
     </div>
   );
